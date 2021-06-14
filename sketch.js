ca669886f581998037302681fceca56798176c36
MOHAKGINNYMITTAL <72095785+MOHAKGINNYMITTAL@users.noreply.github.com>
2021-06-14 19:57:19 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var ball;
 
 function preload()
 {
-	
+	paperimg=loadImage("paper.png")
 }
 
 function setup() {
@@ -15,7 +16,7 @@ function setup() {
 	ground=createSprite(400,300,800,10);
 	dustbin1 =createSprite(600,220,10,150);
 	dustbin2 =createSprite(700,220,10,150);
-
+	ball=createSprite(400,280,20,20);
 
 	engine = Engine.create();
 	world = engine.world;
@@ -31,6 +32,12 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+  ball.setAnimation(paperimg);
+  if (keyDown("space")){
+	  ball.y=+1
+	  ball.x=650
+	  ball.y=260
+  }
   
   drawSprites();
 }
